c9818c7b1d5cff8aadb2ae0c17523cd7a6523b55
rcavazos <rcavazosg@gmail.com>
2026-01-01 20:21:47 -0600

    init creado

--- src/tests.py
+++ src/tests.py
@@ -1,2 +1,6 @@
-def test_suma_correcta(): # Pytest SÍ ve esto
-    assert 2 + 2 == 4
+from main import Calculator # Importamos tu clase desde main.py
+
+def test_suma_calculator():
+    calc = Calculator()
+    # Ahora probamos tu código real
+    assert calc.sum(10, 5) == 15
